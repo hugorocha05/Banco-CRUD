f1346b75906a71c74118bb39f56ccebb72fc329e
hugorocha05 <har3@cesar.school>
2023-05-10 16:02:16 -0300

    Fiz a lógica do extrato, tanto em ordem de adição das transações como agrupadas por categoria

--- Trabalho Módulo 02/Trabalho.py
+++ Trabalho Módulo 02/Trabalho.py
@@ -2,22 +2,23 @@ import os
 os.system('cls')
 
 def pedir_dados():
-    nome = input("\nDigite o nome da pessoa relacionada a essa transação: ")
-    data = input("Digite a data em que a transação ocorreu (no formato dd/mm/aaaa): ")
-    categoria = input("Digite a categoria dessa transação: ")
+    nome = input("\nDigite o nome da pessoa relacionada a essa transação: ").title().strip()
+    data = input("Digite a data em que a transação ocorreu (no formato dd/mm/aaaa): ").strip()
+    categoria = input("Digite a categoria dessa transação: ").title().strip()
     valor = float(input("Digite o valor da transação: R$ "))
     print()
 
     return nome, data, categoria, valor
 
 def guardar_dados(nome, data, categoria, valor):
-    with open('Dados.csv', 'a') as a:  # f = open('Dados.csv', 'a')
+    with open('Trabalho Módulo 02\Dados.csv', 'a') as a:  # f = open('Dados.csv', 'a')
         a.write(f"{nome}, {data}, {categoria}, {valor}\n")
 
 def extrato(agrupado=False):
-    print(f"\n{'-' * 20} Extrato {'-' * 20}")
+    print(f"\n{'=' * 20} Extrato {'=' * 20}")
+    soma_total = 0
 
-    with open('Dados.csv', 'r') as r:
+    with open('Trabalho Módulo 02\Dados.csv', 'r') as r:
         dados = r.readlines()
 
     lista = []
@@ -27,11 +28,30 @@ def extrato(agrupado=False):
 
     if agrupado == False:
         for i in range(len(lista)):
+            soma_total += float(lista[i][3])
             print(f"{i+1})\nNome: {lista[i][0]}\nData: {lista[i][1]}\nCategoria: {lista[i][2]}\nValor: R$ {float(lista[i][3]) :.2f}\n")
     
     elif agrupado == True:
-        # Fazer um sistema de extrato agrupado por categoria
-        pass
+        dic = {}
+        for i in range(len(lista)):  # lista[i][2] == categoria da transação
+            dic[lista[i][2]] = [lista[i]]  # lista[i] é uma transação completa
+            for j in range(len(lista)):
+                if i != j and lista[i][2] == lista[j][2]:
+                    dic[lista[i][2]].append(lista[j])
+        
+        for chave, valor in dic.items():
+            soma_categoria = 0
+            print(f"\n*{chave}*\n")
+            for i in range(len(valor)):
+                soma_total += float(valor[i][3])
+                soma_categoria += float(valor[i][3])
+                print(f"Nome: {valor[i][0]}\nData: {valor[i][1]}\nCategoria: {valor[i][2]}\nValor: R$ {float(valor[i][3]) :.2f}\n")
+                
+            print(f"Extrato da categoria {chave}: R$ {soma_categoria :.2f}")
+            print(f"{'-' * 47}")
+
+    print(f"\nExtrato total das transações: R$ {soma_total :.2f}\n")
+    print(f"{'=' * 47}")
 
 while True:  # Pergunta qual serviço ousuário deseja realisar e ramifica o código para a situação adequada
     opcao = int(input("""[1] Adicionar transação
@@ -41,6 +61,8 @@ while True:  # Pergunta qual serviço ousuário deseja realisar e ramifica o có
 [5] Sair do programa
 Que tipo de serviço você deseja realizar?: """))
     
+
+
     if opcao == 1:  # Sistema de adição de transações
         while True:
             tipo_transacao = int(input("""\n[1] Entrada de Dinheiro
@@ -59,28 +81,44 @@ Que tipo de transação você deseja adicionar?: """))
                 break
             else:
                 print("Escolha inválida, por favor tente novamente.\n")
+        break
+
+
 
     elif opcao == 2:
         while True:
-            tipo_extrato = print("""[1] Mostrar extrato em ordem de adição das transações
-[2] Mostrar extrato agrupado por categoria""")
+            tipo_extrato = int(input("""\n[1] Mostrar extrato em ordem de adição das transações
+[2] Mostrar extrato agrupado por categoria
+Que tipo de extrato você quer?: """))
             
             if tipo_extrato == 1:
                 extrato()
+                break
             elif tipo_extrato == 2:
                 extrato(agrupado=True)
+                break
             else:
                 print("Escolha inválida, por favor tente novamente.\n")
+        break
+
+
 
     elif opcao == 3:
         # Criar sistema de atualização de transações passadas
         pass
 
+
+
     elif opcao == 4:
         # Criar sistema de deleção de transações passadas
         pass
     
+
+
     elif opcao == 5:
         break
+
+
+    
     else:
         print("Escolha inválida, por favor tente novamente.\n")
