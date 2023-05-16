2181287e362f528086e7cb02b13a3f0cea6fc3e4
hugorocha05 <132574651+hugorocha05@users.noreply.github.com>
2023-05-16 10:31:28 -0300

    Adicionei agrupamento das transações por nome, data e categoria
    Opção 2 (mostrar transações passadas) finalizada

--- Trabalho Módulo 02/Trabalho.py
+++ Trabalho Módulo 02/Trabalho.py
@@ -14,43 +14,60 @@ def guardar_dados(nome, data, categoria, valor):
     with open('Trabalho Módulo 02\Dados.csv', 'a') as a:  # f = open('Dados.csv', 'a')
         a.write(f"{nome}, {data}, {categoria}, {valor}\n")
 
-def extrato(agrupado=False):
-    print(f"\n{'=' * 20} Extrato {'=' * 20}")
+def extrato(agrupar=None):
+    print(f"\n{'=' * 20} Extrato {'=' * 20}\n")
     soma_total = 0
 
     with open('Trabalho Módulo 02\Dados.csv', 'r') as r:
         dados = r.readlines()
 
-    lista = []
+    matriz = []
     for i in range(len(dados)):
         dados[i] = dados[i].strip()
-        lista.append(dados[i].split(', '))
-
-    if agrupado == False:
-        for i in range(len(lista)):
-            soma_total += float(lista[i][3])
-            print(f"{i+1})\nNome: {lista[i][0]}\nData: {lista[i][1]}\nCategoria: {lista[i][2]}\nValor: R$ {float(lista[i][3]) :.2f}\n")
-    
-    elif agrupado == True:
-        dic = {}
-        for i in range(len(lista)):  # lista[i][2] == categoria da transação
-            dic[lista[i][2]] = [lista[i]]  # lista[i] é uma transação completa
-            for j in range(len(lista)):
-                if i != j and lista[i][2] == lista[j][2]:
-                    dic[lista[i][2]].append(lista[j])
+        matriz.append(dados[i].split(', '))
         
-        for chave, valor in dic.items():
-            soma_categoria = 0
-            print(f"\n*{chave}*\n")
-            for i in range(len(valor)):
-                soma_total += float(valor[i][3])
-                soma_categoria += float(valor[i][3])
-                print(f"Nome: {valor[i][0]}\nData: {valor[i][1]}\nCategoria: {valor[i][2]}\nValor: R$ {float(valor[i][3]) :.2f}\n")
-                
-            print(f"Extrato da categoria {chave}: R$ {soma_categoria :.2f}")
-            print(f"{'-' * 47}")
+    dic = {}
 
-    print(f"\nExtrato total das transações: R$ {soma_total :.2f}\n")
+    if agrupar == None:
+        for i in range(len(matriz)):
+            soma_total += float(matriz[i][3])
+            print(f"{i+1})\nNome: {matriz[i][0]}\nData: {matriz[i][1]}\nCategoria: {matriz[i][2]}\nValor: R$ {float(matriz[i][3]) :.2f}\n")
+        print()
+
+
+    elif agrupar == "nome":
+        for i in range(len(matriz)):  # matriz[i][0] == nome relacionado à da transação
+            dic[matriz[i][0]] = [matriz[i]]  # matriz[i] é uma transação completa
+            for j in range(len(matriz)):
+                if i != j and matriz[i][0] == matriz[j][0]:
+                    dic[matriz[i][0]].append(matriz[j])
+
+    elif agrupar == "data":
+        for i in range(len(matriz)):  # matriz[i][1] == data da transação
+            dic[matriz[i][1]] = [matriz[i]]  # matriz[i] é uma transação completa
+            for j in range(len(matriz)):
+                if i != j and matriz[i][1] == matriz[j][1]:
+                    dic[matriz[i][1]].append(matriz[j])
+
+    elif agrupar == "categoria":
+        for i in range(len(matriz)):  # matriz[i][2] == categoria da transação
+            dic[matriz[i][2]] = [matriz[i]]  # matriz[i] é uma transação completa
+            for j in range(len(matriz)):
+                if i != j and matriz[i][2] == matriz[j][2]:
+                    dic[matriz[i][2]].append(matriz[j])
+        
+    for chave, valor in dic.items():
+        soma_categoria = 0
+        print(f"*{chave}*\n")
+        for i in range(len(valor)):
+            soma_total += float(valor[i][3])
+            soma_categoria += float(valor[i][3])
+            print(f"Nome: {valor[i][0]}\nData: {valor[i][1]}\nCategoria: {valor[i][2]}\nValor: R$ {float(valor[i][3]) :.2f}\n")
+                
+        print(f"Extrato {agrupar} '{chave}': R$ {soma_categoria :.2f}\n")
+        print(f"{'-' * 47}\n")
+
+    print(f"Extrato total das transações: R$ {soma_total :.2f}\n")
     print(f"{'=' * 47}")
 
 while True:  # Pergunta qual serviço ousuário deseja realisar e ramifica o código para a situação adequada
@@ -88,14 +105,22 @@ Que tipo de transação você deseja adicionar?: """))
     elif opcao == 2:
         while True:
             tipo_extrato = int(input("""\n[1] Mostrar extrato em ordem de adição das transações
-[2] Mostrar extrato agrupado por categoria
+[2] Mostrar extrato agrupado por nome
+[3] Mostrar extrato agrupado por data
+[4] Mostrar extrato agrupado por categoria
 Que tipo de extrato você quer?: """))
             
             if tipo_extrato == 1:
                 extrato()
                 break
             elif tipo_extrato == 2:
-                extrato(agrupado=True)
+                extrato(agrupar='nome')
+                break
+            elif tipo_extrato == 3:
+                extrato(agrupar='data')
+                break
+            elif tipo_extrato == 4:
+                extrato(agrupar='categoria')
                 break
             else:
                 print("Escolha inválida, por favor tente novamente.\n")
